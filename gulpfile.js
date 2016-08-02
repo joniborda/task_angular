7caaaa95b5212212a82bb7dd1d574c2ffc99ee24
joniborda <jonathanmatiasborda@gmail.com>
2016-08-02 09:21:02 -0300

    agrego archivo de config de gulp

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+var gulp = require('gulp'),
+	concat = require('gulp-concat'),
+	uglify = require('gulp-uglify'),
+	minify = require('gulp-minify-css');
+
+gulp.task('scripts', function() {
+  gulp.src([
+  		'./bower_components/jquery/jquery.js',
+  		'./bower_components/angular/angular.js',
+  		'./bower_components/angular-resource/angular-resource.js',
+  		'./bower_components/angular-route/angular-route.js',
+  		'./bower_components/lumx/dist/lumx.js',
+  		'./scripts/**'
+	])
+    .pipe(concat('all.js'))
+   // .pipe(uglify())
+    .pipe(gulp.dest('./dist/js/'))
+});
+
+gulp.task('css', function() {
+  gulp.src([
+  		'./css/main.css',
+  		'./bower_components/lumx/dist/lumx.css'
+	])
+    .pipe(concat('all.css'))
+    .pipe(minify())
+    .pipe(gulp.dest('./dist/css/'))
+});
