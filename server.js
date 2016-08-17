c94e59f06ed56a86dc15ad25615975e7e0b63ab6
joniborda <jonathanmatiasborda@gmail.com>
2016-08-17 10:45:13 -0300

    agrego server para ejecutar todo desde npm

---
+++ server.js
@@ -0,0 +1,30 @@
+var express = require("express"),
+//	bodyParser = require("body-parser"),
+	app = express(),
+	mongoose = require('mongoose'),
+	Schema = mongoose.Schema,
+	taskSchemaJSON = {
+		title: String,
+		description: String
+	};
+
+mongoose.connect("mongodb://localhost/task");
+var task_schema = new Schema(taskSchemaJSON);
+var Task = mongoose.model("Task", task_schema);
+
+app.use("/", express.static("public"));
+
+app.get("/api", function(req, res) {
+
+
+	var task = new Task({
+		title: $scope.post.title,
+		description: $scope.post.description
+	});
+
+	task.save(function() {
+		res.send("Guardamos tus datos");	
+	});
+});
+
+app.listen(80);
